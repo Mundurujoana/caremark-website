e0de78501243bbb191c7053c752751b3b20fe7b5
Mundurujoana <mundujoana@gmail.com>
2022-10-31 05:59:59 +0300

    added changes to the caremark website

--- src/components/Main/MainSection.js
+++ src/components/Main/MainSection.js
@@ -9,7 +9,7 @@ const MainSection = () => {
 
       <div className="intro">
    <h1 data-aos="zoom-out-down">Care Mark</h1>
-   <p data-aos="fade-up">Tuberculosis (TB) is the fourth leading cause of death in Kenya. Despite most forms of TB being curable, one of the main barriers to recovery includes lack of adherence to the long and complicated treatment regimens that involve the daily intake of medicines over the course of 6-24 months.</p>
+   <p data-aos="fade-up">Tuberculosis (TB) is the fourth leading cause of death in Kenya. Despite most forms of TB being curable,<br/> one of the main barriers to recovery includes lack of adherence to the long and complicated treatment regimens <br/> that involve the daily intake of medicines over the course of 6-24 months.</p>
    <p data-aos="fade-up">We at CareMark are at a Mission to Save Lives By Delivering Powerful Behavioral Interventions Across Mobile Phones.</p>
   
    <Link to='works' className='btn' activeClass="active"
